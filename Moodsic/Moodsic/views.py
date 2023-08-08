e3f384c3d426f17c272950c024b6421668a8fcdf
Guilherme Vassallo <guilhermevassallo09@gmail.com>
2023-08-08 19:33:46 -0300

    Impl. básica do reconhecimento de sentimentos

--- Moodsic/Moodsic/views.py
+++ Moodsic/Moodsic/views.py
@@ -1,5 +1,17 @@
 # Create your views here.
 from django.shortcuts import render
+from transformers import pipeline
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
 
 def homepage(request):
+    if request.method == 'POST':
+        user_input = request.POST.get('user_input')
+        prediction = classifier(user_input)
+        print(prediction)
+        
+        return render(request, 'Moodsic/index.html')
+
+    classifier = pipeline("text-classification", model='bhadresh-savani/distilbert-base-uncased-emotion', return_all_scores=True)
     return render(request, 'Moodsic/index.html')
