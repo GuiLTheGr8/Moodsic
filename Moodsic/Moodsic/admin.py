d4a032984ff361ba61246286b1ff8363446e1d1f
Guilherme Vassallo <guilhermevassallo09@gmail.com>
2023-09-22 18:39:07 -0300

    Bootstrap + CSS inicial

--- Moodsic/Moodsic/admin.py
+++ Moodsic/Moodsic/admin.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 from.models import Playlist, Moodsic
 
 # Register your models here.
-admin.site.register(Playlist)
-admin.site.register(Moodsic)
+# admin.site.register(Playlist)
+# admin.site.register(Moodsic)
