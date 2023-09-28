89ab3c6926c74ad15e2501257aa41ea3ad9a95e9
Guilherme Vassallo <guilhermevassallo09@gmail.com>
2023-09-28 19:18:55 -0300

    Estilização completa homepage / privacidade

--- Moodsic/Moodsic/admin.py
+++ Moodsic/Moodsic/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from.models import Playlist, Moodsic
 
+class MoodsicAdmin(admin.ModelAdmin):
+    exclude = ('typedText',)
+
 # Register your models here.
-# admin.site.register(Playlist)
-# admin.site.register(Moodsic)
+admin.site.register(Playlist)
+admin.site.register(Moodsic, MoodsicAdmin)
